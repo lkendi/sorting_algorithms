c2d3a57ab3deeac2884f73fc64f5a04bd8b8a48b
lkendi <lkendi003@gmail.com>
2024-02-23 11:56:19 +0300

    Add selection sort files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,5 +8,29 @@
 
 void selection_sort(int *array, size_t size)
 {
+	int min_index, temp;
+	size_t i, j;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		/*Traverse the list to find the current min*/
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+
+		/*Swap current min with the first unsorted element*/
+		if (array[min_index] != array[i])
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+		i++;
+	}
 
 }
