c3fdd32e49f8d58c512e2b4c2b648c1a3fa693f0
lkendi <lkendi003@gmail.com>
2024-02-23 11:40:38 +0300

    Fix betty errors

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,11 +9,11 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 
 	current = (*list)->next;
-	while(current != NULL)
+	while (current != NULL)
 	{
 		current = (*list)->next;
 		while (current != NULL)
